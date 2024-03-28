3a118d604f0e218a7a68d8f718c6705dd6e02daa
paulmatthew lazarte <paulmatthew708@gmail.com>
2024-03-27 23:49:29 -0700

    Wine Project

---
+++ Python Projects/Wine - Linear Regression Model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+
+wine_df = pd.read_csv('data/Wine.csv')
+wineTest_df = pd.read_csv('data/WineTest.csv')
+
